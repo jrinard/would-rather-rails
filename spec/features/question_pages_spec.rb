ce7b8a64a509df2270c30e449b1e9282a9a8755f
Jim Padilla and Joshua Rinard <jim+joshua@>
2017-02-21 15:28:10 -0800

    more tests

--- spec/features/question_pages_spec.rb
+++ spec/features/question_pages_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+
+describe "Add Clothing" do
+  it "Create and View Clothing" do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    click_on 'New Question'
+    fill_in 'Option one', :with => 'taco'
+    fill_in 'Option two', :with => 'taco'
+    page.attach_file("Image", 'spec/images/test.jpg')
+    page.attach_file("Image", 'spec/images/test2.jpg')
+    click_on 'Create Question'
+    expect(page).to have_content 'taco'
+  end
+end
